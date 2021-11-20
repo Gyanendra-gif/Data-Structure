7b4aca950483f58da7392b86f84416fcc8cba449
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-11-20 11:29:39 +0530

    [Gyanendra] Add: A Program Data Addition on Linked List

--- Data Structure/Data Structure/LinkedList.cs
+++ Data Structure/Data Structure/LinkedList.cs
@@ -7,7 +7,7 @@ namespace Data_Structure
    public  class LinkedList
     {
         internal Node head;
-        internal void Add(int data) 
+        internal void Add(int data) // This Method Addind Data into the List
         {
             Node node = new Node(data);
             if (this.head == null)
@@ -24,6 +24,20 @@ namespace Data_Structure
                 temp.next = node;
             }
             Console.WriteLine("{0} Inserted into Linked List", node.data);
+        }
+        internal void Display() // This Method Showing the Data in the List
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked List is Empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
         }
     }
 }
--- Data Structure/Data Structure/Program.cs
+++ Data Structure/Data Structure/Program.cs
@@ -10,6 +10,7 @@ namespace Data_Structure
             ln.Add(56); 
             ln.Add(30);
             ln.Add(70);
+            ln.Display();
         }
     }
 }
