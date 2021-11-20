507d1ee75067230616fa1d54b57e5762132de7da
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-11-20 11:09:52 +0530

    [Gyanendra] Add: A Program of Linked Lidt Creation

---
+++ Data Structure/Data Structure/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Data_Structure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList ln = new LinkedList();
+            ln.Add(56); 
+            ln.Add(30);
+            ln.Add(70);
+        }
+    }
+}
