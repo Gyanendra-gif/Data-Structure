3c013b7c44978cb93e63213559f02195aed32d47
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-11-20 11:38:36 +0530

    [Gyanendra] Add: A Program to Add Data on Linked List at Particular Position

--- Data Structure/Data Structure/Program.cs
+++ Data Structure/Data Structure/Program.cs
@@ -7,9 +7,10 @@ namespace Data_Structure
         static void Main(string[] args)
         {
             LinkedList ln = new LinkedList();
-            ln.Add(56); 
+            ln.Add(56);
             ln.Add(30);
             ln.Add(70);
+            ln.InsertAtParticularPosition(2, 30);
             ln.Display();
         }
     }
